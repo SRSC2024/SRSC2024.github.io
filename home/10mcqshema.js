8ba35c27e958f87d995781153fa0dad752d4f125
omar desouki <149262536+SRSC2024@users.noreply.github.com>
2023-11-02 15:23:43 +0300

    up

---
+++ home/10mcqshema.js
@@ -0,0 +1,230 @@
+// Define your questions
+const allQuestions = [
+    {
+        question: "1",
+        answers: [
+            { text: "yes", correct: false },
+            { text: "no", correct: true },
+            { text: "mabye", correct: false },
+            { text: "absulutly", correct: false }
+        ]
+    },
+    {
+        question: "2",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "3",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "4",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "5",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "6?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "7",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "8",
+        answers: [
+            { text: "yes", correct: false },
+            { text: "no", correct: true },
+            { text: "mabye", correct: false },
+            { text: "absulutly", correct: false }
+        ]
+    },
+    {
+        question: "9",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "10",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "11",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "12",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "13",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    },
+    {
+        question: "14",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false }
+        ]
+    }
+];
+
+// Shuffle function
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Number of questions to ask
+const numQuestionsToAsk = 10;
+
+// Shuffle your questions
+shuffleArray(allQuestions);
+
+// Select the first 10 questions
+const questions = allQuestions.slice(0, numQuestionsToAsk);
+
+// Your existing code for the quiz
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${numQuestionsToAsk}!`;
+    nextButton.innerHTML = "RETRY";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < numQuestionsToAsk) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < numQuestionsToAsk) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
--- home/script.js
+++ home/script.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', function (event
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'yourusername' && password === 'yourpassword') {
+    if (username === '1' && password === '1') {
         window.location.href = 'dashboard.html';
     } else {
         document.getElementById('message').textContent = 'Invalid credentials. Please try again.';

36162df97d872ae69f6f6f1a8bc9fec681adc7b0
omar desouki <149262536+SRSC2024@users.noreply.github.com>
2023-11-02 15:36:27 +0300

    0

--- home/scriptindex.js
+++ home/scriptindex.js
@@ -1,10 +1,10 @@
 document.getElementById('login-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
 
-    if (username === 'omar' && password === 'omar') {
+    if (username.toLowerCase() === 'omar' && password.toLowerCase() === 'omar') {
         window.location.href = 'dashboard.html';
     } else {
         document.getElementById('message').textContent = 'Invalid credentials. Please try again.';
